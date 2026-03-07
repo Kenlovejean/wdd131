40f28f39b9e3307004829749716c38682cfe69a1
kenlovejean <jjules1@byupathway.edu>
2026-03-07 04:52:10 -0600

    homepage week01

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const year = document.querySelector("#currentyear");
+
+year.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+"Last Modified: " + document.lastModified;
